4b3702a5b61849c8da1a295a169033ff1dc3b329
bbowler982 <78939899+bbowler982@users.noreply.github.com>
2021-03-08 15:00:03 -0500

    Initial Commit
    Adding project with oculus integration setup and a few scripts added

---
+++ TestUnityProject/Assets/Oculus/VR/Scripts/AttachedToCamera.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachedToCamera : MonoBehaviour
+{
+    [SerializeField]
+    public Transform cameraTransform;
+    [SerializeField]
+    Vector3 offsetFromCamera;
+    [SerializeField]
+    float turnBufferInDegrees = 30f;
+    [SerializeField]
+    float turnSpeed = 30f;
+    [SerializeField]
+    bool lockPitch = false;
+    public bool locked = true;
+   
+
+    GameObject obj;
+
+    
+    void Start()
+    {
+        
+        transform.localPosition = cameraTransform.localPosition + offsetFromCamera;
+        //Vector3 cameraEuler = cameraTransform.rotation.eulerAngles;
+
+        //cameraEuler.z = 0f;
+
+        /*if(lockPitch)
+        {
+            cameraEuler.x = 0f;
+        }*/
+
+       // Quaternion targetQuat = Quaternion.Euler(cameraEuler);
+
+        //float angle = Quaternion.Angle(transform.rotation, targetQuat);
+
+        /*if(angle > turnBufferInDegrees)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetQuat, turnSpeed);
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        obj = GameObject.Find("Cube Chair");
+        OVRGrabbable check = obj.GetComponent <OVRGrabbable>();
+        
+        bool grabbedOnce = check.isGrabbed;
+        if(grabbedOnce == true)
+        {
+            locked = false;
+        }
+
+        if (locked == true)
+        {
+            transform.localPosition = cameraTransform.localPosition + offsetFromCamera;
+        }
+
+        //transform.localPosition = cameraTransform.localPosition + offsetFromCamera;
+
+    }
+}
---
+++ TestUnityProject/Assets/Scripts/FPSJump.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSJump : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float jumpForce;
+    [SerializeField] Transform groundChecker;
+    [SerializeField] float checkRadius;
+    [SerializeField] LayerMask groundLayer;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && IsOnGround())
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    bool IsOnGround()
+    {
+        Collider[] colliders = Physics.OverlapSphere(groundChecker.position, checkRadius, groundLayer);
+
+        if (colliders.Length > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+}
